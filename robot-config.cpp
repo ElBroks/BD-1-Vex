0923bdc8a3940388008da66d56d29153621e5c20
BroKoliXx <112986402+ElBroks@users.noreply.github.com>
2023-02-20 16:07:05 -0600

    Add files via upload
    Version mejorada, ya solo faltan rodillos

---
+++ robot-config.cpp
@@ -0,0 +1,46 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+
+motor LadoDerechoMotorA = motor(PORT1, ratio18_1, false);
+motor LadoDerechoMotorB = motor(PORT2, ratio18_1, true);
+motor LadoDerechoMotorC = motor(PORT3, ratio18_1, true);
+motor LadoDerechoMotorD = motor(PORT4, ratio18_1, false);
+motor_group LadoDerecho = motor_group(LadoDerechoMotorA, LadoDerechoMotorB,LadoDerechoMotorC,LadoDerechoMotorD);
+
+motor LadoIzquierdoMotorA = motor(PORT11, ratio18_1, true);
+motor LadoIzquierdoMotorB = motor(PORT12, ratio18_1, false);
+motor LadoIzquierdoMotorC = motor(PORT13, ratio18_1, false);
+motor LadoIzquierdoMotorD = motor(PORT14, ratio18_1, true);
+motor_group LadoIzquierdo = motor_group(LadoIzquierdoMotorA, LadoIzquierdoMotorB,LadoIzquierdoMotorC, LadoIzquierdoMotorD);
+
+motor Recogedor = motor(PORT9, ratio18_1, false);
+
+motor ServoLanzador = motor(PORT5, ratio18_1,false);
+
+motor MotorLanzador1 = motor(PORT7, ratio18_1, false);
+motor MotorLanzador2 = motor(PORT8, ratio18_1, true);
+motor_group Lanzador = motor_group(MotorLanzador1,MotorLanzador2);
+
+
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
